596c13880cb373cf082ff61edd47b1f2e1e718ef
sarita595k <sarita595k@gmail.com>
2026-09-15 16:56:25 +0530

    route link added

--- src/route/customerRoute.js
+++ src/route/customerRoute.js
@@ -2,15 +2,16 @@ import express from "express"
 import { createCustomer, deleteCustomer, getAllCustomers, updateCustomer } from "../controller/customerController.js"
 const route = express.Router()
 
-// route for create a customer
+// route for create a customer /api/customer/create
 route.post("/create", createCustomer)
 
-// route to get the list of all the customers 
+// route to get the list of all the customers  /api/customer/getAll
 route.get("/getAll", getAllCustomers)
-// route for update a customer 
+
+// route for update a customer /api/customer/update/id
 route.put("/update/:id", updateCustomer)
 
-// route to detete a customer
+// route to detete a customer /api/customer/delete/id
 route.delete("/delete/:id", deleteCustomer)
 
 export default route
